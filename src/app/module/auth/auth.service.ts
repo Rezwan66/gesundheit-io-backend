424245d0c3af19b0fc6e056e5ef088001a1ef0e4
Rezwan66 <shaikhrezwan66@gmail.com>
2026-02-15 13:31:18 +0100

    feat(auth): implement auth with social login, auth checks and access, refresh tokens

--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -4,12 +4,15 @@ import AppError from '../../errorHelpers/AppError';
 import { auth } from '../../lib/auth';
 import { prisma } from '../../lib/prisma';
 import { tokenUtils } from '../../utils/token';
-
-interface IRegisterPatientPayload {
-  name: string;
-  email: string;
-  password: string;
-}
+import { IRequestUser } from '../../interfaces/requestUser.interface';
+import { jwtUtils } from '../../utils/jwt';
+import { envVars } from '../../config/env';
+import { JwtPayload } from 'jsonwebtoken';
+import {
+  IChangePasswordPayload,
+  ILoginUserPayload,
+  IRegisterPatientPayload,
+} from './auth.interface';
 
 const registerPatient = async (payload: IRegisterPatientPayload) => {
   const { name, email, password } = payload;
@@ -74,11 +77,6 @@ const registerPatient = async (payload: IRegisterPatientPayload) => {
   }
 };
 
-interface ILoginUserPayload {
-  email: string;
-  password: string;
-}
-
 const loginUser = async (payload: ILoginUserPayload) => {
   const { email, password } = payload;
 
@@ -125,4 +123,274 @@ const loginUser = async (payload: ILoginUserPayload) => {
   return { ...data, accessToken, refreshToken };
 };
 
-export const AuthService = { registerPatient, loginUser };
+const getMe = async (user: IRequestUser) => {
+  const isUserExist = await prisma.user.findUnique({
+    where: {
+      id: user.userId,
+    },
+    include: {
+      patient: {
+        include: {
+          appointments: true,
+          reviews: true,
+          prescriptions: true,
+          medicalReports: true,
+          patientHealthData: true,
+        },
+      },
+      doctor: {
+        include: {
+          specialties: true,
+          appointments: true,
+          reviews: true,
+          prescriptions: true,
+        },
+      },
+      admin: true,
+    },
+  });
+
+  if (!isUserExist) {
+    throw new AppError(status.NOT_FOUND, 'User not found');
+  }
+
+  return isUserExist;
+};
+
+const getNewToken = async (refreshToken: string, sessionToken: string) => {
+  const isSessionTokenExists = await prisma.session.findUnique({
+    where: {
+      token: sessionToken,
+    },
+    include: {
+      user: true,
+    },
+  });
+
+  if (!isSessionTokenExists) {
+    throw new AppError(status.UNAUTHORIZED, 'Invalid session token');
+  }
+
+  const verifiedRefreshToken = jwtUtils.verifyToken(
+    refreshToken,
+    envVars.REFRESH_TOKEN_SECRET,
+  );
+  if (!verifiedRefreshToken.success && verifiedRefreshToken.error) {
+    throw new AppError(status.UNAUTHORIZED, 'Invalid refresh token');
+  }
+  const data = verifiedRefreshToken.data as JwtPayload;
+
+  const newAccessToken = tokenUtils.getAccessToken({
+    userId: data.userId,
+    role: data.role,
+    name: data.name,
+    email: data.email,
+    status: data.status,
+    isDeleted: data.isDeleted,
+    emailVerified: data.emailVerified,
+  });
+
+  const newRefreshToken = tokenUtils.getRefreshToken({
+    userId: data.userId,
+    role: data.role,
+    name: data.name,
+    email: data.email,
+    status: data.status,
+    isDeleted: data.isDeleted,
+    emailVerified: data.emailVerified,
+  });
+
+  // Increase validity of better auth session token
+  const { token } = await prisma.session.update({
+    where: {
+      token: sessionToken,
+    },
+    data: {
+      token: sessionToken, // Keep the same session token
+      expiresAt: new Date(Date.now() + 60 * 60 * 60 * 24 * 1000), // Extend session by 1 day
+      updatedAt: new Date(),
+    },
+  });
+
+  return {
+    accessToken: newAccessToken,
+    refreshToken: newRefreshToken,
+    sessionToken: token,
+  };
+};
+
+const changePassword = async (
+  payload: IChangePasswordPayload,
+  sessionToken: string,
+) => {
+  const session = await auth.api.getSession({
+    headers: new Headers({
+      Authorization: `Bearer ${sessionToken}`,
+    }),
+  });
+  if (!session) {
+    throw new AppError(status.UNAUTHORIZED, 'Invalid session token');
+  }
+
+  const { currentPassword, newPassword } = payload;
+
+  const result = await auth.api.changePassword({
+    body: {
+      currentPassword,
+      newPassword,
+      revokeOtherSessions: true,
+    },
+    headers: new Headers({
+      Authorization: `Bearer ${sessionToken}`,
+    }),
+  });
+
+  // Only for first time user login when needPasswordChange is true
+  if (session.user.needPasswordChange) {
+    await prisma.user.update({
+      where: { id: session.user.id },
+      data: { needPasswordChange: false },
+    });
+  }
+
+  // Need to set new tokens in cookies after password change as all other sessions are revoked
+  const accessToken = tokenUtils.getAccessToken({
+    userId: session.user.id,
+    role: session.user.role,
+    name: session.user.name,
+    email: session.user.email,
+    status: session.user.status,
+    isDeleted: session.user.isDeleted,
+    emailVerified: session.user.emailVerified,
+  });
+
+  const refreshToken = tokenUtils.getRefreshToken({
+    userId: session.user.id,
+    role: session.user.role,
+    name: session.user.name,
+    email: session.user.email,
+    status: session.user.status,
+    isDeleted: session.user.isDeleted,
+    emailVerified: session.user.emailVerified,
+  });
+
+  return { ...result, accessToken, refreshToken };
+};
+
+const logoutUser = async (sessionToken: string) => {
+  return await auth.api.signOut({
+    headers: new Headers({
+      Authorization: `Bearer ${sessionToken}`,
+    }),
+  });
+};
+
+const verifyEmail = async (email: string, otp: string) => {
+  const result = await auth.api.verifyEmailOTP({
+    body: { email, otp },
+  });
+
+  if (result.status && !result.user.emailVerified) {
+    await prisma.user.update({
+      where: { email },
+      data: { emailVerified: true },
+    });
+  }
+};
+
+const forgetPassword = async (email: string) => {
+  const isUserExist = await prisma.user.findUnique({
+    where: { email },
+  });
+  if (!isUserExist) {
+    throw new AppError(status.NOT_FOUND, 'User with this email does not exist');
+  }
+  if (!isUserExist.emailVerified) {
+    throw new AppError(status.BAD_REQUEST, 'Email is not verified');
+  }
+  if (isUserExist.isDeleted || isUserExist.status === UserStatus.DELETED) {
+    throw new AppError(status.BAD_REQUEST, 'User is deleted');
+  }
+  await auth.api.requestPasswordResetEmailOTP({
+    body: { email },
+  });
+};
+
+const resetPassword = async (
+  email: string,
+  otp: string,
+  newPassword: string,
+) => {
+  const isUserExist = await prisma.user.findUnique({
+    where: { email },
+  });
+  if (!isUserExist) {
+    throw new AppError(status.NOT_FOUND, 'User with this email does not exist');
+  }
+  if (!isUserExist.emailVerified) {
+    throw new AppError(status.BAD_REQUEST, 'Email is not verified');
+  }
+  if (isUserExist.isDeleted || isUserExist.status === UserStatus.DELETED) {
+    throw new AppError(status.BAD_REQUEST, 'User is deleted');
+  }
+
+  await auth.api.resetPasswordEmailOTP({
+    body: { email, otp, password: newPassword },
+  });
+
+  // Only for first time user login when needPasswordChange is true
+  if (isUserExist.needPasswordChange) {
+    await prisma.user.update({
+      where: { id: isUserExist.id },
+      data: { needPasswordChange: false },
+    });
+  }
+
+  await prisma.session.deleteMany({
+    where: {
+      userId: isUserExist.id,
+    },
+  });
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const googleLoginSuccess = async (session: Record<string, any>) => {
+  const isPatientExists = await prisma.patient.findUnique({
+    where: { userId: session.user.id },
+  });
+  if (!isPatientExists) {
+    await prisma.patient.create({
+      data: {
+        userId: session.user.id,
+        name: session.user.name,
+        email: session.user.email,
+      },
+    });
+  }
+
+  const accessToken = tokenUtils.getAccessToken({
+    userId: session.user.id,
+    role: session.user.role,
+    name: session.user.name,
+  });
+  const refreshToken = tokenUtils.getRefreshToken({
+    userId: session.user.id,
+    role: session.user.role,
+    name: session.user.name,
+  });
+
+  return { accessToken, refreshToken };
+};
+
+export const AuthService = {
+  registerPatient,
+  loginUser,
+  getMe,
+  getNewToken,
+  changePassword,
+  logoutUser,
+  verifyEmail,
+  forgetPassword,
+  resetPassword,
+  googleLoginSuccess,
+};
--- src/app/utils/token.ts
+++ src/app/utils/token.ts
@@ -32,7 +32,7 @@ const setAccessTokenCookie = (res: Response, token: string) => {
     secure: true,
     sameSite: 'none',
     path: '/',
-    maxAge: 60 * 60 * 60 * 24, // 1 day
+    maxAge: 60 * 60 * 24 * 1000, // 1 day
   });
 };
 
@@ -43,7 +43,7 @@ const setRefreshTokenCookie = (res: Response, token: string) => {
     secure: true,
     sameSite: 'none',
     path: '/',
-    maxAge: 60 * 60 * 60 * 24 * 7, // 7 days
+    maxAge: 60 * 60 * 24 * 1000 * 7, // 7 days
   });
 };
 
@@ -54,7 +54,7 @@ const setBetterAuthSessionCookie = (res: Response, token: string) => {
     secure: true,
     sameSite: 'none',
     path: '/',
-    maxAge: 60 * 60 * 60 * 24, // 1 day
+    maxAge: 60 * 60 * 24 * 1000, // 1 day
   });
 };
 
